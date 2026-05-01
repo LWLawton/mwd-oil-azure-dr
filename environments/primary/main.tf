6f28c1508d5397a860ca0d27a77a54cfab077034
Loren Lawton <Loren.lawton@gmail.com>
2026-05-01 13:02:06 -0400

    Create main.tf
    bug fix 1.6

---
+++ environments/primary/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "primary" {
+  name     = "rg-mwd-primary-demo"
+  location = "South Central US"
+
+  tags = {
+    project     = "mwd-oil-azure-dr"
+    environment = "primary"
+    owner       = "security-ops"
+  }
+}
